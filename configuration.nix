4719f669a02f949dcb95f3f64fa98714367dceeb
Nikita Iashchenko <nikita.yaschenko@gmail.com>
2023-12-13 18:26:33 +0000

    Enable weekly GC

--- configuration.nix
+++ configuration.nix
@@ -15,6 +15,20 @@
   boot.loader.systemd-boot.configurationLimit = 10;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  # GC weekly to lower disk usage
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 1w";
+  };
+
+  # Optimize storage
+  # You can also manually optimize the store via:
+  #    nix-store --optimise
+  # Refer to the following link for more details:
+  # https://nixos.org/manual/nix/stable/command-ref/conf-file.html#conf-auto-optimise-store
+  nix.settings.auto-optimise-store = true;
+
   networking.hostName = "pc"; # Define your hostname.
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
