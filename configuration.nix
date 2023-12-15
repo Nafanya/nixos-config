493ea2745f4e02dad981565cfd85207c21de3f7f
Nikita Iashchenko <nikita.yaschenko@gmail.com>
2023-12-15 17:41:59 +0000

    Enable sound with pipewire

--- configuration.nix
+++ configuration.nix
@@ -95,6 +95,14 @@
 
   programs.sway.enable = true;
 
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
