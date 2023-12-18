0a5450c338156b626d4146242271862a32f1278f
Nikita Iashchenko <nikita.yaschenko@gmail.com>
2023-12-18 19:00:47 +0000

    Add XDG_CURRENT_DESKTOP option for firefox+wayland

--- home.nix
+++ home.nix
@@ -37,6 +37,10 @@
     enable = true;
   };
 
+  home.sessionVariables = {
+    XDG_CURRENT_DESKTOP = "sway";
+  };
+
   # Packages that should be installed to the user profile.
   home.packages = with pkgs; [
     # here is some command line tools I use frequently
