3cc125e16db2ebfc7ba797bef5f9d4d11dc78fe2
Nikita Iashchenko <nikita.yaschenko@gmail.com>
2023-12-19 01:19:37 +0000

    Add some env vars

--- configuration.nix
+++ configuration.nix
@@ -108,6 +108,14 @@
   ];
 
   environment.variables.EDITOR = "vim";
+  environment.sessionVariables = {
+    WLR_NO_HARDWARE_CURSORS = "1";
+    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    GBM_BACKEND = "nvidia-drm";
+    XDG_SESSION_TYPE = "wayland";
+    LIBVA_DRIVER_NAME = "nvidia";
+    MOZ_ENABLE_WAYLAND = "1";
+  };
 
   programs.hyprland.enable = true;
 
