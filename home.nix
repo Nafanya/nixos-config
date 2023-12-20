48422ffb7a6df58b27c37c1252b323bb5410dc7b
Nikita Iashchenko <nikita.yaschenko@gmail.com>
2023-12-20 13:06:58 +0000

    Switch kb layouts with capslock

--- home.nix
+++ home.nix
@@ -54,7 +54,7 @@
           kb_layout = us,ru
           kb_variant =
           kb_model =
-          kb_options =
+          kb_options = grp:caps_toggle
           kb_rules =
       
           follow_mouse = 1
