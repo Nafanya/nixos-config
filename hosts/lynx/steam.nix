23e6da5e470eaa4a03b4b14a98a4a91cc069e6ac
Nikita Iashchenko <nikita.yaschenko@gmail.com>
2024-01-07 13:25:42 +0000

    Add steam user with steam pkg

--- hosts/lynx/steam.nix
+++ hosts/lynx/steam.nix
@@ -1,6 +1,14 @@
 { config, pkgs, ... }:
 
 {
-  environment.systemPackages = [ pkgs.steamPackages.steamcmd ];
+  users.users.steam = {
+    isNormalUser = true;
+    description = "Steam runner";
+    packages = with pkgs; [ steam ];
+  };
 
+  #programs.steam = {
+  #  enable = true;
+  #  dedicatedServer.openFirewall = true;
+  #};
 }
