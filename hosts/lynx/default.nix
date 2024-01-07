c50c272dfcfe2eb10941fc633357a387f9ebdc40
Nikita Iashchenko <nikita.yaschenko@gmail.com>
2024-01-07 12:45:03 +0000

    Some ssh...

--- hosts/lynx/default.nix
+++ hosts/lynx/default.nix
@@ -140,7 +140,7 @@
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
-  # services.openssh.permitRootLogin = "no";
+  services.openssh.permitRootLogin = "no";
 
   services.blueman.enable = true;
 
