d8b9461c1732ad247eaafe0a887ea405f3575d50
Nikita Iashchenko <nikita.yaschenko@gmail.com>
2024-01-29 20:58:29 +0000

    i3: add workspace assignments

--- hosts/pc/i3/default.nix
+++ hosts/pc/i3/default.nix
@@ -1,4 +1,7 @@
 { config, pkgs, ... }: {
 
-  xdg.configFile = { "i3/config".source = ./config; };
+  xdg.configFile = {
+    "i3/config".source = ./config;
+    "i3/assignments.conf".source = ./assignments.conf;
+  };
 }
