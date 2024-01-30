12db245ac9d71a1ce00d1b9843a72eb834ec4012
Nikita Iashchenko <nikita.yaschenko@gmail.com>
2024-01-30 16:49:20 +0000

    i3: add polybar

--- hosts/pc/i3/default.nix
+++ hosts/pc/i3/default.nix
@@ -3,5 +3,16 @@
   xdg.configFile = {
     "i3/config".source = ./config;
     "i3/assignments.conf".source = ./assignments.conf;
+    "polybar/config".source = ./polybar/config;
+  };
+
+  services.polybar = {
+    enable = true;
+    package = pkgs.polybarFull;
+    #TODO: use systemd for logs / startup / restart
+    script = ''
+      polybar-msg cmd quit
+      polybar top 2>&1 | tee -a /tmp/polybar-top.log & disown
+    '';
   };
 }
