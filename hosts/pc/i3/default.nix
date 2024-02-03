905ac733b079b27dc6f4838066b344754ea8468f
Nikita Iashchenko <nikita.yaschenko@gmail.com>
2024-02-03 18:06:29 +0000

    i3: disable polybar

--- hosts/pc/i3/default.nix
+++ hosts/pc/i3/default.nix
@@ -3,14 +3,5 @@
   xdg.configFile = {
     "i3/config".source = ./config;
     "i3/assignments.conf".source = ./assignments.conf;
-    "polybar/config.ini".source = ./polybar/config.ini;
-  };
-
-  services.polybar = {
-    enable = true;
-    package = pkgs.polybarFull;
-    script = ''
-      polybar top &
-    '';
   };
 }
