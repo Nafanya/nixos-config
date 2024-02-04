7a30c57490366858be79a036ff30564142d9db7f
Nikita Iashchenko <nikita.yaschenko@gmail.com>
2024-02-04 01:15:15 +0000

    gtk: enable catpuccin-macchiato theme

--- hosts/pc/home.nix
+++ hosts/pc/home.nix
@@ -108,6 +108,19 @@
     obs-studio
   ];
 
+  gtk = {
+    enable = true;
+    theme = {
+      name = "Catppuccin-Macchiato-Compact-Pink-Dark";
+      package = pkgs.catppuccin-gtk.override {
+        accents = [ "pink" ];
+        size = "compact";
+        tweaks = [ "rimless" "black" ];
+        variant = "macchiato";
+      };
+    };
+  };
+
   services.easyeffects.enable = true;
 
   # kitty -- a cross-platform, GPU-accelerated terminal emulator
