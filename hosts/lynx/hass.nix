1acf65396cc0d564295e197d7506f90d5e11fee4
Nikita Iashchenko <nikita.yaschenko@gmail.com>
2024-02-22 16:39:13 +0000

    nginx: hass works

--- hosts/lynx/hass.nix
+++ hosts/lynx/hass.nix
@@ -9,22 +9,24 @@
       "radio_browser"
     ];
     extraPackages = python3Packages: with python3Packages; [ securetar ];
-    config = {
-      # Includes dependencies for a basic setup
-      # https://www.home-assistant.io/integrations/default_config/
-      default_config = { };
-      http = {
-        server_host = "::1";
-        trusted_proxies = [ "::1" ];
-        use_x_forwarded_for = true;
-      };
+  };
+
+  services.home-assistant.config = {
+    # Includes dependencies for a basic setup
+    # https://www.home-assistant.io/integrations/default_config/
+    default_config = { };
+    http = {
+      trusted_proxies = [ "::1" "127.0.0.1" ];
+      use_x_forwarded_for = true;
     };
   };
 
   services.nginx = {
     virtualHosts."hass.local" = {
       locations."/" = {
-        proxyPass = "http://[::1]:8123";
+        proxyPass = "http://127.0.0.1:8123";
+        proxyWebsockets = true;
+        recommendedProxySettings = true;
         extraConfig = ''
           proxy_buffering off;
         '';
