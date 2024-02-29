d0b1cfbf441e81fa4208cef441651fa4bba339f8
Nikita Iashchenko <nikita.yaschenko@gmail.com>
2024-02-29 12:51:09 +0000

    Uncomment nix.package = pkgs.nixUnstable + refactor[C

--- flake.nix
+++ flake.nix
@@ -3,8 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nix-darwin.url = "github:LnL7/nix-darwin";
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-darwin = {
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     home-manager = {
       url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
--- hosts/leopard/default.nix
+++ hosts/leopard/default.nix
@@ -1,18 +1,18 @@
 { pkgs, inputs, ... }: {
 
-  environment.systemPackages = [
-    pkgs.vim
-    pkgs.git
+  environment.systemPackages = with pkgs; [
+    vim
+    git
   ];
 
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
-  # nix.package = pkgs.nix;
 
-  # Necessary for using flakes on this system.
-  nix.settings.experimental-features = "nix-command flakes";
-
-  # Create /etc/zshrc that loads the nix-darwin environment.
+  nix = {
+    package = pkgs.nixUnstable;
+    settings.experimental-features = "nix-command flakes";
+  };
+  # Create /etc/zshrc that loads the nix-arwin environment.
   programs.zsh.enable = true;  # default shell on catalina
   # programs.fish.enable = true;
 
