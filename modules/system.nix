6db4c8d8d841ab54bbb56d2bfd426cc2fc40ab61
Nikita Iashchenko <nikita.yaschenko@gmail.com>
2024-03-02 13:06:08 +0000

    Add remote build key

--- modules/system.nix
+++ modules/system.nix
@@ -6,6 +6,7 @@
     extraGroups = [ "wheel" "gamemode" ]; # TODO: cleanup gamemode?
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJnSWkY3hvd27BbsNGDRkeL2/XDY1d96Szsz79P0lXCH nikita.yaschenko@gmail.com"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINTeQWq1hmUPwDBvMVeWD9xa/c9IBvo2q+uxNXWySC2e nikita.yaschenko@gmail.com"
     ];
     shell = pkgs.zsh; # TODO: remove from here and configure in hm?
   };
