8cfe014a429dff0f082017dfdde57d61b36ff0fe
Nikita Iashchenko <nikita.yaschenko@gmail.com>
2024-03-15 14:39:09 +0000

    Position 2nd monitor right of 1st

--- hosts/pc/modules/i3.nix
+++ hosts/pc/modules/i3.nix
@@ -13,7 +13,8 @@
         rofi # app launcher, instead of dmenu
       ];
       extraSessionCommands = ''
-        xrandr --output DP-2 --mode 2560x1440 --rate 144.00
+        xrandr --output DP-2 --primary --mode 2560x1440 --rate 144.00
+        xrandr --output DP-4 --right-of DP-2 --rotate right
       '';
     };
     xkb = {
