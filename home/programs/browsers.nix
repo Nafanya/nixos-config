accc3d0a6f61e966bb719f380beaf1ae2e2acde5
Nikita Iashchenko <nikita.yaschenko@gmail.com>
2024-03-16 12:16:38 +0000

    Add firefox containers

--- home/programs/browsers.nix
+++ home/programs/browsers.nix
@@ -1,7 +1,32 @@
 { pkgs, config, ... }: {
   programs.firefox = {
     enable = true;
-    #TODO: separate profiles for sensitive sites
-    profiles.nikita = { };
+    profiles.nikita = {
+      containers = {
+        personal = {
+          id = 0;
+          name = "Personal";
+          color = "pink";
+          icon = "fingerprint";
+        };
+        work = {
+          id = 1;
+          name = "Work";
+          color = "orange";
+          icon = "briefcase";
+        };
+        banking = {
+          id = 2;
+          name = "Banking";
+          color = "green";
+          icon = "dollar";
+        };
+        shopping = {
+          id = 3;
+          name = "Shopping";
+          color = "turquoise";
+          icon = "cart";
+        };
+      };
   };
 }
