0d2ece082d2c06e0e38c37ac2226f92ded0d56a4
Nikita Iashchenko <nikita.yaschenko@gmail.com>
2024-03-16 12:18:37 +0000

    Add firefox search engines

--- home/programs/browsers.nix
+++ home/programs/browsers.nix
@@ -28,5 +28,54 @@
           icon = "cart";
         };
       };
+      search = {
+        engines = {
+          "Nix Packages" = {
+            urls = [{
+              template = "https://search.nixos.org/packages";
+              params = [
+                {
+                  name = "type";
+                  value = "packages";
+                }
+                {
+                  name = "query";
+                  value = "{searchTerms}";
+                }
+              ];
+            }];
+            icon =
+              "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+            definedAliases = [ "@np" ];
+          };
+          "NixOS Wiki" = {
+            urls = [{
+              template = "https://nixos.wiki/index.php?search={searchTerms}";
+            }];
+            icon =
+              "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+            definedAliases = [ "@nw" ];
+          };
+          "NixOS Options" = {
+            urls = [{
+              template = "https://search.nixos.org/options?query={searchTerms}";
+            }];
+            icon =
+              "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+            definedAliases = [ "@no" ];
+          };
+          "Home Manager Options" = {
+            urls = [{
+              template =
+                "https://home-manager-options.extranix.com/?query={searchTerms}";
+            }];
+            definedAliases = [ "@hmo" ];
+          };
+          "Bing".metaData.hidden = true;
+          "Google".metaData.alias = "@g";
+          # builtin engines only support specifying one additional alias}
+        };
+      };
+    };
   };
 }
