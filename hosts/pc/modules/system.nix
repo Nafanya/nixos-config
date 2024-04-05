442bccd6a5f8e9c3cf48cdf0cd092d213a22d2a2
Nikita Iashchenko <nikita.yaschenko@gmail.com>
2024-04-05 10:56:29 +0100

    Disable jack (sound)

--- hosts/pc/modules/system.nix
+++ hosts/pc/modules/system.nix
@@ -95,7 +95,6 @@
       audio.enable = true;
       pulse.enable = true;
       wireplumber.enable = true;
-      jack.enable = true;
       alsa = {
         enable = true;
         support32Bit = true;
