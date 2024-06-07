64bf68ff0dcee4bb4af53087e14fe443b7d9334a
Nikita Iashchenko <nikita.yaschenko@gmail.com>
2024-06-07 12:25:18 +0100

    Switch to 24.05 from unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,8 @@
   description = "Nikita's NixOS Flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     nix-darwin = {
       url = "github:LnL7/nix-darwin";
@@ -12,7 +13,7 @@
     sops-nix.url = "github:Mic92/sops-nix";
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
