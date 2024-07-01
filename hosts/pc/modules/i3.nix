656c9d42f64dc46097a6f6df94c652f5953de19d
Nikita Iashchenko <nikita.yaschenko@gmail.com>
2024-07-01 22:29:01 +0100

    Disable i3; enable sddm+plasma

--- hosts/pc/modules/i3.nix
+++ hosts/pc/modules/i3.nix
@@ -1,23 +1,12 @@
 { config, pkgs, ... }: {
   services.xserver = {
     enable = true;
-    displayManager = { defaultSession = "none+i3"; };
-    desktopManager.plasma6.enable = true;
-    windowManager.i3 = {
-      enable = true;
-      extraPackages = with pkgs; [
-        dunst # notifcation daemon
-        feh # set wallpaper
-        i3blocks
-        i3lock
-        i3status
-        rofi # app launcher, instead of dmenu
-      ];
-      extraSessionCommands = ''
-        xrandr --output DP-2 --primary --mode 2560x1440 --rate 144.00
-        xrandr --output DP-4 --right-of DP-2 --rotate right
-      '';
+    displayManager = {
+      defaultSession = "plasma";
+      sddm.enable = true;
+      sddm.wayland.enable = true;
     };
+    desktopManager.plasma6.enable = true;
     xkb = {
       options = "grp:caps_toggle";
       layout = "us,ru";
