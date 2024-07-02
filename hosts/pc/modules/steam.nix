7f3147675a96f4f1aed1a9da74bd66c58fd9db10
Nikita Iashchenko <nikita.yaschenko@gmail.com>
2024-07-02 22:10:25 +0100

    for gamescope

--- hosts/pc/modules/steam.nix
+++ hosts/pc/modules/steam.nix
@@ -4,6 +4,18 @@
     enable = true;
     remotePlay = { openFirewall = true; };
     gamescopeSession.enable = true;
+    extraPackages = with pkgs; [
+      xorg.libXcursor
+      xorg.libXi
+      xorg.libXinerama
+      xorg.libXScrnSaver
+      libpng
+      libpulseaudio
+      libvorbis
+      stdenv.cc.cc.lib
+      libkrb5
+      keyutils
+    ];
   };
 
   hardware.steam-hardware.enable = true;
