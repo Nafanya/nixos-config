be538a989ac7b31c88f9b8a874cfc7b556ff5dd7
Nikita Iashchenko <nikita.yaschenko@gmail.com>
2024-07-02 21:27:29 +0100

    enable gamescope

--- hosts/pc/modules/steam.nix
+++ hosts/pc/modules/steam.nix
@@ -3,7 +3,10 @@
   programs.steam = {
     enable = true;
     remotePlay = { openFirewall = true; };
+    gamescopeSession.enable = true;
   };
 
   hardware.steam-hardware.enable = true;
+
+  programs.gamescope.enable = true;
 }
