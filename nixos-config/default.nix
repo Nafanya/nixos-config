a93865474e62f3a966e2042df9c73970cd584306
Nikita Iashchenko <nikita.yaschenko@gmail.com>
2024-07-09 16:32:19 +0100

    refactor: move fonts

--- nixos-config/default.nix
+++ nixos-config/default.nix
@@ -81,4 +81,22 @@
     nano.enable = false;
     zsh.enable = true;
   };
+
+  fonts = {
+    enableDefaultPackages = true;
+    packages = with pkgs; [
+      font-awesome
+      powerline-fonts
+      powerline-symbols
+      source-code-pro
+      (nerdfonts.override {
+        fonts = [
+          "SpaceMono"
+          "JetBrainsMono"
+          "DejaVuSansMono"
+          "SourceCodePro"
+        ];
+      })
+    ];
+  };
 }
--- nixos-config/hosts/pc-old/modules/system.nix
+++ nixos-config/hosts/pc-old/modules/system.nix
@@ -13,24 +13,6 @@
 
   i18n.defaultLocale = "en_GB.UTF-8";
 
-  fonts = {
-    enableDefaultPackages = true;
-    packages = with pkgs; [
-      font-awesome
-      powerline-fonts
-      powerline-symbols
-      source-code-pro
-      (nerdfonts.override {
-        fonts = [
-          "SpaceMono"
-          "JetBrainsMono"
-          "DejaVuSansMono"
-          "SourceCodePro"
-        ];
-      })
-    ];
-  };
-
   networking.firewall.enable = true;
 
   services.openssh = {
