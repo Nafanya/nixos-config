271cbd0aded63ba6627beb9ca75ee93c977df81a
Nikita Iashchenko <nikita.yaschenko@gmail.com>
2024-07-09 18:15:31 +0100

    refactor: hm: move the rest

--- home-config/config/applications/default.nix
+++ home-config/config/applications/default.nix
@@ -9,6 +9,45 @@
   ];
 
   home.packages = with pkgs; [
-    # add here
+    aria2
+    bat
+    btop
+    file
+    fzf
+    neofetch
+    ripgrep
+    tree
+    unzip
+    which
+    zip
+
+    # nix
+    home-manager
+
+    #misc
+    ffmpeg
+    spotify
+    instaloader
+
+    # system tools
+    ethtool
+    iotop
+    lm_sensors # for `sensors`
+    pciutils # for `lspci`
+    sysstat
+    usbutils # for `lsusb`
+    nvtopPackages.full
+
+    # im
+    discord
+    telegram-desktop
+
+    #misc
+    obs-studio
+
+    # audio
+    pavucontrol
+    playerctl
+    qjackctl
   ];
 }
