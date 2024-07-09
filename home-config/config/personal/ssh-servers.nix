c3be0b97bc4ee544b85db7dd4d2d722085e6364e
Nikita Iashchenko <nikita.yaschenko@gmail.com>
2024-07-09 19:06:26 +0100

    refactor: add ssh-servers

--- home-config/config/personal/ssh-servers.nix
+++ home-config/config/personal/ssh-servers.nix
@@ -3,10 +3,28 @@
   #TODO: Setup ssh keys properly
   #home.file.".ssh/nikitoci.pub".source = "${flake-inputs.self}/keys/nikitoci.pub";
 
-  #programs.ssh.matchBlocks = {
-  #  "*" = {
-  #    identitiesOnly = true;
-  #    identityFile = "~/.ssh/nikitoci.pub";
-  #  };
-  #};
+  programs.ssh.matchBlocks = {
+    #"*" = {
+    #  identitiesOnly = true;
+    #  identityFile = "~/.ssh/nikitoci.pub";
+    #};
+    "lynx" = {
+      hostname = "lynx.local";
+      user = "nikita";
+      identityFile = "~/.ssh/lynx";
+    };
+    "leopard" = {
+      hostname = "leopard.local";
+      user = "nikita";
+      identityFile = "~/.ssh/id_pc_leopard";
+    };
+    "github.com" = {
+      identityFile = "~/.ssh/pc-github";
+    };
+    "nihonzaru" = {
+      hostname = "167.235.29.144";
+      user = "root";
+      identityFile = "~/.ssh/id_pc@hetzner_nikitoci.com";
+    };
+  };
 }
