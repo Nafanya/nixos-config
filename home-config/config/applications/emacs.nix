65a3994391d0d0b6276f6eed88a4f2d5e4a221e9
Nikita Iashchenko <nikita.yaschenko@gmail.com>
2024-07-09 18:49:34 +0100

    refactor: fix: use default emacs package

--- home-config/config/applications/emacs.nix
+++ home-config/config/applications/emacs.nix
@@ -1,34 +1,25 @@
 { pkgs, flake-inputs, ... }:
-let
-  inherit (flake-inputs) self;
-  inherit (flake-inputs.self.packages.${pkgs.system}) emacs;
-  inherit (flake-inputs.nixd.packages.${pkgs.system}) nixd;
-in
 {
-  home.packages =
-    (with pkgs; [
-      (aspellWithDicts (
-        dicts: with dicts; [
-          en
-          en-computers
-          ru
-          ua
-        ]
-      ))
+  home.packages = with pkgs; [
+    (aspellWithDicts (
+      dicts: with dicts; [
+        en
+        en-computers
+        ru
+      ]
+    ))
 
-      nixfmt-rfc-style
+    nixfmt-rfc-style
 
-      # markdown
-      pandoc
+    # markdown
+    pandoc
 
-      libnotify
-      sqlite.dev
-    ])
-    ++ [ nixd ];
+    libnotify
+    sqlite.dev
+  ];
 
   programs.emacs = {
     enable = true;
-    package = emacs;
   };
 
   services.emacs = {
