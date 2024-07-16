17c638f082386d8e4bcf8e589c4df289a3a60076
Nikita Iashchenko <nikita.yaschenko@gmail.com>
2024-07-16 18:05:52 +0100

    install smartmontools and plasma-disks

--- nixos-config/hosts/pc/smart.nix
+++ nixos-config/hosts/pc/smart.nix
@@ -5,7 +5,18 @@
   ...
 }:
 {
-  environment.systemPackages = with pkgs; [ smartmontools ];
+  environment.systemPackages = with pkgs; [
+    smartmontools
+    kdePackages.plasma-disks
+  ];
 
   services.smartd.enable = true;
+
+  hardware.sensor.hddtemp = {
+    enable = true;
+    drives = [
+      "/dev/sda"
+      "/dev/sdb"
+    ];
+  };
 }
