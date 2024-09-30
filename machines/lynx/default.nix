b52ee6a328e28fe88b92d31decbc52f7eda347e2
Nikita Iashchenko <nikita.yaschenko@gmail.com>
2024-09-30 16:42:43 +0100

    Remove networking.hostName as it's already set by flake

--- machines/lynx/default.nix
+++ machines/lynx/default.nix
@@ -28,13 +28,6 @@
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
 
-  networking.hostName = "lynx"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
   # Wayland + sway
   programs.sway.enable = true;
 
