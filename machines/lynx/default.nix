3d1c0eaa8df8062c0beb4fcdcd7ec5b06c4212f6
Nikita Iashchenko <nikita.yaschenko@gmail.com>
2024-09-30 16:32:31 +0100

    Extract locale settings to profile

--- machines/lynx/default.nix
+++ machines/lynx/default.nix
@@ -16,7 +16,7 @@
 
     #### NEW defs ####
     inputs.sops-nix.nixosModules.sops
-  ];
+  ] ++ (with inputs.self.nixosModules.profiles; [ locale ]);
 
   sops.defaultSopsFile = ../../hosts/lynx/secrets.yaml;
 
@@ -35,21 +35,6 @@
 
   # Set your time zone.
   time.timeZone = "Europe/London";
-
-  i18n = {
-    defaultLocale = "en_GB.UTF-8";
-    extraLocaleSettings = {
-      LC_ADDRESS = "en_GB.UTF-8";
-      LC_IDENTIFICATION = "en_GB.UTF-8";
-      LC_MEASUREMENT = "en_GB.UTF-8";
-      LC_MONETARY = "en_GB.UTF-8";
-      LC_NAME = "en_GB.UTF-8";
-      LC_NUMERIC = "en_GB.UTF-8";
-      LC_PAPER = "en_GB.UTF-8";
-      LC_TELEPHONE = "en_GB.UTF-8";
-      LC_TIME = "en_GB.UTF-8";
-    };
-  };
 
   # Wayland + sway
   programs.sway.enable = true;
