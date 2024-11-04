558ff730afc8ae648fb69999f568ab6653f2ee6f
Nikita Iashchenko <nikita.yaschenko@gmail.com>
2024-11-04 15:56:12 +0000

    Revert "Disable onlySSL for website"
    This reverts commit 369c8596e033e8d5c4f9e27f190fea0e1431334b.

--- profiles/server/website.nix
+++ profiles/server/website.nix
@@ -3,6 +3,7 @@
   services.nginx = {
     virtualHosts."nikitoci.com" = {
       useACMEHost = "nikitoci.com";
+      onlySSL = true;
       root = "/srv/www/nikitoci.com";
       locations."/" = {
         index = "index.html";
