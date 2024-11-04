369c8596e033e8d5c4f9e27f190fea0e1431334b
Nikita Iashchenko <nikita.yaschenko@gmail.com>
2024-11-04 12:51:56 +0000

    Disable onlySSL for website

--- profiles/server/website.nix
+++ profiles/server/website.nix
@@ -3,7 +3,6 @@
   services.nginx = {
     virtualHosts."nikitoci.com" = {
       useACMEHost = "nikitoci.com";
-      onlySSL = true;
       root = "/srv/www/nikitoci.com";
       locations."/" = {
         index = "index.html";
