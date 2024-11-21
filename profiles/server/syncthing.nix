b0b5f944a3805e0106204ee880045ce6c2dd078c
Nikita Iashchenko <nikita.yaschenko@gmail.com>
2024-11-21 12:31:48 +0000

    Enable syncthing

--- profiles/server/syncthing.nix
+++ profiles/server/syncthing.nix
@@ -3,8 +3,18 @@
   services.syncthing = {
     enable = true;
     user = "nikita";
-    dataDir = "/mnt/data/sync";
     configDir = "/home/nikita/.config/syncthing";
     openDefaultPorts = true;
+    folders = {
+      "Documents" = {
+        path = "/mnt/data/documents";
+      };
+      "Obsidian" = {
+        path = "/mnt/data/obsidian";
+      };
+    };
   };
+
+  # Don't create default ~/Sync folder
+  systemd.services.syncthing.environment.STNODEFAULTFOLDER = "true";
 }
