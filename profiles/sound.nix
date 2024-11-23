0763ef5d8456dba7cbc3371d9ad2703516c782a4
Nikita Iashchenko <nikita.yaschenko@gmail.com>
2024-11-23 15:59:50 +0000

    Remove low-latency audio

--- profiles/sound.nix
+++ profiles/sound.nix
@@ -12,37 +12,6 @@
       support32Bit = true;
     };
     extraConfig = {
-      pipewire."92-low-latency" = {
-        "context.properties" = {
-          "default.clock.allowed-rates" = [
-            44100
-            48000
-            88200
-            96000
-          ];
-          "default.clock.quantum" = 32;
-          "default.clock.min-quantum" = 32;
-          "default.clock.max-quantum" = 32;
-        };
-      };
-      pipewire-pulse."92-low-latency" = {
-        "context.modules" = [
-          {
-            name = "libpipewire-module-protocol-pulse";
-            args = {
-              "pulse.min.req" = "32/48000";
-              "pulse.default.req" = "32/48000";
-              "pulse.max.req" = "32/48000";
-              "pulse.min.quantum" = "32/48000";
-              "pulse.max.quantum" = "32/48000";
-            };
-          }
-        ];
-        "stream.properties" = {
-          "node.latency" = "32/48000";
-          "resample.quality" = 1;
-        };
-      };
       pipewire."95-obs-sink" = {
         "context.objects" = [
           {
