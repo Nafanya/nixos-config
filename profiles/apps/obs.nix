6082748fac0a2d50ff8b4222e1ccb5315f53e47b
Nikita Iashchenko <nikita.yaschenko@gmail.com>
2024-11-23 16:04:45 +0000

    Add OBS virtual cam

--- profiles/apps/obs.nix
+++ profiles/apps/obs.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 {
   programs.obs-studio = {
     enable = true;
@@ -9,4 +9,13 @@
       obs-vkcapture
     ];
   };
+
+  boot.extraModulePackages = with config.boot.kernelPackages; [
+    v4l2loopback
+  ];
+  boot.kernelModules = [ "v4l2loopback" ];
+  boot.extraModprobeConfig = ''
+    options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+  '';
+  security.polkit.enable = true;
 }
