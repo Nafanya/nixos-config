2e599592812317b451bd43ba574e93975983338b
Nikita Iashchenko <nikita.yaschenko@gmail.com>
2024-11-26 16:01:19 +0000

    Add v4l2 cameras

--- profiles/apps/obs.nix
+++ profiles/apps/obs.nix
@@ -15,7 +15,11 @@
   ];
   boot.kernelModules = [ "v4l2loopback" ];
   boot.extraModprobeConfig = ''
-    options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+    options v4l2loopback devices=3 video_nr=5,6,7 card_label="Web Camera,OBS Camera,Discord Camera" exclusive_caps=1,1,1
   '';
   security.polkit.enable = true;
+
+  # Copy camera feed to three v4l2 devices:
+  # ffmpeg -f v4l2 -input_format mjpeg -framerate 60 -video_size 1920x1080 -i /dev/video0 -c:v copy \
+  #        -f tee -map 0:v "[f=v4l2]/dev/video5|[f=v4l2]/dev/video6|[f=v4l2]/dev/video7"
 }
