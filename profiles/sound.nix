d88663eb86c8f273ad2b03b7ab92ecd185287c8f
Nikita Iashchenko <nikita.yaschenko@gmail.com>
2024-11-27 11:09:55 +0000

    Disable HDMI output

--- profiles/sound.nix
+++ profiles/sound.nix
@@ -5,7 +5,27 @@
   services.pipewire = {
     enable = true;
     pulse.enable = true;
-    wireplumber.enable = true;
+    wireplumber = {
+      enable = true;
+      extraConfig = {
+        "51-disable-radeon-hdmi" = {
+          "monitor.alsa.rules" = [
+            {
+              matches = [
+                {
+                  "device.name" = "alsa_card.pci-0000_03_00.1";
+                }
+              ];
+              actions = {
+                update-props = {
+                  "device.disabled" = true;
+                };
+              };
+            }
+          ];
+        };
+      };
+    };
     jack.enable = true;
     alsa = {
       enable = true;
