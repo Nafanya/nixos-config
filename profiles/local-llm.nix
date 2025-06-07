9ca25d32e101d145be0c33ceadff38bf62202379
Nikita Iashchenko <nikita.yaschenko@gmail.com>
2025-06-07 10:39:56 +0100

    Add llama-cpp service (disabled)

--- profiles/local-llm.nix
+++ profiles/local-llm.nix
@@ -15,4 +15,23 @@
     })
   ];
 
+  services.llama-cpp = {
+    enable = false;
+    package = pkgs.llama-cpp.override {
+      rocmSupport = true;
+      vulkanSupport = true;
+      openclSupport = true;
+    };
+    host = "0.0.0.0";
+    port = 8080;
+    openFirewall = true;
+    model = "/mnt/data/downloads/unsloth_Qwen3-30B-A3B-GGUF_Qwen3-30B-A3B-UD-IQ1_S.gguf";
+    extraFlags = [
+      "--flash-attn"
+      "--gpu-layers"
+      "99"
+      "--device"
+      "Vulkan0"
+    ];
+  };
 }
