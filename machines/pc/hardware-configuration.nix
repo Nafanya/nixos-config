f9d22d1decfd90aa65a8502da455acb6e2426715
Nikita Iashchenko <nikita.yaschenko@gmail.com>
2025-06-07 12:20:43 +0100

    Update flake to 07-06-2025; use 6_14 kernel

--- machines/pc/hardware-configuration.nix
+++ machines/pc/hardware-configuration.nix
@@ -20,7 +20,7 @@
     "usb_storage"
     "sd_mod"
   ];
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackages_6_14;
   boot.initrd.kernelModules = [ "dm-snapshot" ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.blacklistedKernelModules = [ ];
