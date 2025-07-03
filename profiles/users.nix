71b12b0fd067ed6425a24ce1249689c9b731d611
Nikita Iashchenko <nikita.yaschenko@gmail.com>
2025-07-03 17:58:48 +0100

    Add cf ssh key

--- profiles/users.nix
+++ profiles/users.nix
@@ -20,6 +20,7 @@
         openssh.authorizedKeys.keys = [
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG0rNQhiU4hXq45bbQk6H0RqGZ1cMTuMc5a3yGpfnTkP nikita.yaschenko@gmail.com"
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBD/7H19xdHEMBJDQp2FWtgUduTx6ZW6xghZJsQEpU6O nikita.yaschenko@gmail.com"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO4zMr2LItR1a2D3v+ouTwGMJubUS1naON/RSGPJguTp nikita.yaschenko@gmail.com" # cf
         ];
       };
     };
